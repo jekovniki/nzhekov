dda4641c479e761a12ff688cb62cd568fb1c8c4c
Nikolay <jekovniki95@gmail.com>
2021-04-17 18:34:55 +0300

    About content

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -5,7 +5,31 @@ import MainLayout from '../../components/mainLayout';
 const About = () => {
     return (
         <MainLayout>
-            <h1>About us</h1>
+            <section className={styles.page}>
+                <div className={styles.text}>
+                    <div className={styles.container}>
+                        <h2>
+                            Me, Myself &amp; I
+                        </h2>
+                        <p>
+                            I'm a Front-End Developer located in Bulgaria. I have a serious passion for UI effects, animations
+                            and creative intuitive, dynamic user experiences.
+                        </p>
+                        <p>
+                            Well-organised person, problem solver, independent employee with high attention to detail. Fan ot all kinds
+                            of sports(from ice hockey to motorsports), outdoor activities, video games, TV series and history books.
+                        </p>
+                        <p>
+                            Interested in the entire frontend spectrum and working on ambitious projects with positive people.
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.animation}>
+                    <div className={styles.container}>
+                        Test, test, test, test
+                    </div>
+                </div>
+            </section>
         </MainLayout>
     )
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,9 +9,21 @@ const Homepage = () => {
             <section className={styles.homepage}>
                 <div className={styles.container}>
                     <h1>
-                        Hi,<br/>
-                        I'm <span className={styles.letter}>N</span>iki,
-                        <br/>web developer
+                        <p className={styles.paragraph}>
+                            <p className={styles.letter}>H</p>
+                            <p className={styles.letter}>i</p>
+                            <p className={styles.letter}>,</p>
+                        </p>
+                        <p className={styles.paragraph}>
+                            <p className={styles.letter}>I</p><p className={styles.letter}>'</p>
+                            <p className={styles.letter}>m</p> <p className={styles.letterMain}>N</p>
+                            <p className={styles.letter}>i</p><p className={styles.letter}>k</p>
+                            <p className={styles.letter}>i</p><p className={styles.letter}>,</p>
+                        </p>
+                        <p className={styles.paragraph}>
+                            <p className={styles.letter}>w</p><p className={styles.letter}>e</p>
+                            <p className={styles.letter}>b</p> <p className={styles.word}>developer</p>
+                        </p>
                     </h1>
                     <p>
                         Front End Developer / Drupal Developer / ReactJS / CMS specialist
