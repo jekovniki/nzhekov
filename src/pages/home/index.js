c4e166b4b1ac571bcffb74c2ad6606a8b5cc2ba1
Nikolay <jekovniki95@gmail.com>
2021-04-17 09:49:57 +0300

    Main Teaser homepage done

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,7 +5,23 @@ import MainLayout from '../../components/mainLayout'
 const Homepage = () => {
     return (
         <MainLayout>
-            Homepage
+            <section className={styles.homepage}>
+                <div className={styles.container}>
+                    <h1>
+                        Hi,<br/>
+                        I'm <span className={styles.leter}>N</span>iki,
+                        <br/>web developer
+                    </h1>
+                    <p>
+                        Front End Developer / Drupal Developer / ReactJS / CMS specialist
+                    </p>
+                    <div className={styles.button}>
+                        <a href="/contact">
+                            Contact me!
+                        </a>
+                    </div>
+                </div>
+            </section>
         </MainLayout>
     )
 }
