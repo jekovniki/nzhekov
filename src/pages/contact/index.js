ebe2c8e6546babf9ab671ac85a6c0c96390c97d0
Nikolay <jekovniki95@gmail.com>
2021-04-23 09:58:51 +0300

    Google Maps added to react

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import styles from './index.module.scss';
 import MainLayout from '../../components/mainLayout';
+import SimpleMap from '../../components/googleMaps'
 
 const Contact = () => {
     return (
         <MainLayout>
-            <h1>Contact</h1>
+            <section className={styles.page}>
+                <div className={styles.text}>
+                    <div className={styles.container}>
+                        <h2>
+                            Me, Myself &amp; I
+                        </h2>
+                        <p>
+                            I'm a Front-End Developer located in Bulgaria. I have a serious passion for UI effects, animations
+                            and creative intuitive, dynamic user experiences.
+                        </p>
+                        <p>
+                            Well-organised person, problem solver, independent employee with high attention to detail. Fan to all kinds
+                            of sports(from ice hockey to motorsports), outdoor activities, video games, TV series and history books.
+                        </p>
+                        <p>
+                            Interested in the entire frontend spectrum and working on ambitious projects with positive people.
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.animation}>
+                    <div className={styles.container}>
+                        <SimpleMap />
+                    </div>
+                </div>
+            </section>
         </MainLayout>
     )
 }
