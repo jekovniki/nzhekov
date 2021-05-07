d9b6ec6313acb79111a964c33db9db9f20c17e16
Nikolay <jekovniki95@gmail.com>
2021-05-07 08:43:31 +0300

    Frontpage logo - with svg

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './index.module.scss';
 import MainLayout from '../../components/mainLayout'
-import Logo from '../../images/N.svg'
 
 const Homepage = () => {
     return (
@@ -36,8 +35,17 @@ const Homepage = () => {
                     </div>
                 </div>
             </section>
-            <section>
-                <img src={Logo} width='250px' height='250px' fill='#fff'/>
+            <section className={styles.logo}>
+                <svg x="0px" y="0px" width="33%" viewBox="0 0 23.631 23.631"><g><g>
+		            <polygon 
+                        points="0,0.663 9.401,0.663 15.882,7.146 15.882,14.127 5.307,3.608 5.274,22.969 0,22.969" 
+                        />
+		            <polygon 
+                        points="23.631,22.969 14.232,22.969 7.752,16.485 7.752,9.501 18.327,20.018 18.359,0.662 23.631,0.662"
+                    />
+                    </g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+                    <g></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+                    </svg>
             </section>
         </MainLayout>
         </div>
