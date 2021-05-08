8b09f422ef7520e0fd79c71c9cec89c874371040
Nikolay <jekovniki95@gmail.com>
2021-05-08 15:44:16 +0300

    Projects page animations

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './index.module.scss';
 import MainLayout from '../../components/mainLayout';
 import project1 from '../../images/greenproperty1.png'
+import projectLogo from '../../images/greenproperty-logo.png'
 import project2 from '../../images/newcarsuperstore.png'
 import project3 from '../../images/oxnardautofinance.png'
 
@@ -12,21 +13,21 @@ const Projects = () => {
                 <div className={styles.projects}>
                     <img src={project1} className={styles.img} 
                     alt="Green Property Investments" title="Green Property Investments"/>
-                    <h3>Green Property Investments Inc.</h3>
+                    <div className={styles.view}><div><p>View<br/> project</p></div></div>
                     <div className={styles.content}>
                     </div>
                 </div>
                 <div className={styles.projects}>
                     <img src={project2}  className={styles.img} 
                 alt="New Car Superstore" title="New Car Superstore"/>
-                    <h3>New Car Superstore</h3>
+                    <div className={styles.view}><div><p>View<br/> project</p></div></div>
                     <div className={styles.content}>
                     </div>
                 </div>
                 <div className={styles.projects}>
                     <img src={project3} className={styles.img} 
                 alt="Oxnard Auto Finance" title="Oxnard Auto Finance"/>
-                    <h3>Oxnard Auto Finance</h3>
+                    <div className={styles.view}><div><p>View<br/> project</p></div></div>
                     <div className={styles.content}>
                     </div>
                 </div>
