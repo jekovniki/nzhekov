4b20fc073aedee992043201953c5c14c97dea710
Nikolay <jekovniki95@gmail.com>
2021-05-16 08:37:09 +0300

    Styling changes

--- src/pages/skills/index.js
+++ src/pages/skills/index.js
@@ -5,27 +5,29 @@ import SkillsPageContent from '../../components/skillsPageContent';
 
 const Skills = () => {
     return (
-        <SkillsPageLayout>
-            <SkillsPageContent title="Skills &amp; Experience">
-            <p>
-                Since beginning my journey as a freelance developer nearly 10 years ago, 
-                I've done remote work for agencies, consulted for startups, and collaborated with talented 
-                people to create web products for both business and consumer use.
-            </p>
-            <p>
-                I create successful responsive websites that are fast, easy to use, and built with best practices.
-                The main area of my expertise is front-end developmnet, HTML, CSS, JS, building small and medium
-                web apps, custom plugins, features, animations, and coding interactive layouts.
-            </p>
-            <p>
-                I also have full-stack developer experience with popular open-source CMS like (Wordpress, Drupal,
-                Adobe Experience Manager, Typo3, Shopify, Shopware and others).
-            </p>
-            <p>
-                Visit my LinkedIn profile for more details or just contact me.
-            </p>
-            </SkillsPageContent>
-        </SkillsPageLayout>
+        <div className={styles.skills}>
+            <SkillsPageLayout>
+                <SkillsPageContent title="Skills &amp; Experience">
+                <p>
+                    Since beginning my journey as a freelance developer nearly 10 years ago, 
+                    I've done remote work for agencies, consulted for startups, and collaborated with talented 
+                    people to create web products for both business and consumer use.
+                </p>
+                <p>
+                    I create successful responsive websites that are fast, easy to use, and built with best practices.
+                    The main area of my expertise is front-end developmnet, HTML, CSS, JS, building small and medium
+                    web apps, custom plugins, features, animations, and coding interactive layouts.
+                </p>
+                <p>
+                    I also have full-stack developer experience with popular open-source CMS like (Wordpress, Drupal,
+                    Adobe Experience Manager, Typo3, Shopify, Shopware and others).
+                </p>
+                <p>
+                    Visit my LinkedIn profile for more details or just contact me.
+                </p>
+                </SkillsPageContent>
+            </SkillsPageLayout>
+        </div>
     )
 }
 
