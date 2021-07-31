0a6e5cccd6401e876b5de8130335b63e65396377
Nikolay <jekovniki95@gmail.com>
2021-07-31 08:46:37 +0300

    Grid template for project page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -5,6 +5,14 @@ import project1 from '../../images/greenproperty1.png'
 import projectLogo from '../../images/greenproperty-logo.png'
 import project2 from '../../images/newcarsuperstore.png'
 import project3 from '../../images/oxnardautofinance.png'
+import project4 from '../../images/algeco-ro.png'
+import project5 from '../../images/paramjute.png'
+import project6 from '../../images/juteplanet.png'
+import project7 from '../../images/lyubomislici.jpg'
+
+
+
+
 
 const Projects = () => {
     return (
@@ -13,6 +21,13 @@ const Projects = () => {
                 <div className={styles.projects}>
                     <img src={project1} className={styles.img} 
                     alt="Green Property Investments" title="Green Property Investments"/>
+                    <div className={styles.view}><div><p>View<br/> project</p></div></div>
+                    <div className={styles.content}>
+                    </div>
+                </div>
+                <div className={styles.projects}>
+                    <img src={project4} className={styles.img} 
+                alt="Algeco Romania" title="Algeco Romania"/>
                     <div className={styles.view}><div><p>View<br/> project</p></div></div>
                     <div className={styles.content}>
                     </div>
@@ -27,6 +42,27 @@ const Projects = () => {
                 <div className={styles.projects}>
                     <img src={project3} className={styles.img} 
                 alt="Oxnard Auto Finance" title="Oxnard Auto Finance"/>
+                    <div className={styles.view}><div><p>View<br/> project</p></div></div>
+                    <div className={styles.content}>
+                    </div>
+                </div>
+                <div className={styles.projects}>
+                    <img src={project5} className={styles.img} 
+                alt="Paramjute" title="Paramjute"/>
+                    <div className={styles.view}><div><p>View<br/> project</p></div></div>
+                    <div className={styles.content}>
+                    </div>
+                </div>
+                <div className={styles.projects}>
+                    <img src={project6} className={styles.img} 
+                alt="Juteplanet" title="Juteplanet"/>
+                    <div className={styles.view}><div><p>View<br/> project</p></div></div>
+                    <div className={styles.content}>
+                    </div>
+                </div>
+                <div className={styles.projects}>
+                    <img src={project7} className={styles.img} 
+                alt="Lyubomislici" title="Lyubomislici"/>
                     <div className={styles.view}><div><p>View<br/> project</p></div></div>
                     <div className={styles.content}>
                     </div>
